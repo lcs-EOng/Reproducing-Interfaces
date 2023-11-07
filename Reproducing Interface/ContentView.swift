14b96ef1fddd63113572267a9e8650a06a28a474
Eason Ong <eong@lcs.on.ca>
2023-11-07 14:05:24 -0500

    Initial Commit

---
+++ Reproducing Interface/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Reproducing Interface
+//
+//  Created by Ong Eason on 7/11/2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Reproducing Interface/Reproducing_InterfaceApp.swift
@@ -0,0 +1,17 @@
+//
+//  Reproducing_InterfaceApp.swift
+//  Reproducing Interface
+//
+//  Created by Ong Eason on 7/11/2023.
+//
+
+import SwiftUI
+
+@main
+struct Reproducing_InterfaceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
